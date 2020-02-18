16e33fe5a9757bc17b5e4a94c56548b071d09fed
sunitha123241 <kasaragaddasunitha@gmail.com>
2020-02-18 19:30:54 +0530

    third commit

---
+++ SimpleInterest.java
@@ -0,0 +1,9 @@
+package com.Interest;
+
+public class SimpleInerest {
+
+	float ClaculateSimpleInterest(float principal,float rate,float time_period)
+	{
+		return (principal*rate*time_period)/100;
+	}
+}
