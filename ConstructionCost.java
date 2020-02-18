ece01b9a2f9d768ca89cba5e79f598f928e2950c
sunitha123241 <kasaragaddasunitha@gmail.com>
2020-02-18 22:05:31 +0530

    fifth commit

---
+++ ConstructionCost.java
@@ -0,0 +1,30 @@
+package HouseConstruction;
+
+public class ConstructionCost {
+	
+	float calculateConstructionCost(String material,float no_of_square_feet)
+	{
+		System.out.println(no_of_square_feet);
+		if(material.equals("standard"))
+		{
+			System.out.println(no_of_square_feet);
+			return 1200*no_of_square_feet;
+		}
+		else if(material.equals("above standard"))
+		{
+			return 1500*no_of_square_feet;
+		}
+		else if(material.equals("high standard"))
+		{
+			return 1800*no_of_square_feet;
+		}
+		else if(material.equals("hidh standard and fully automated home"))
+		{
+			return 2500*no_of_square_feet;
+		}
+		else {
+		return 0;
+		}
+	}
+
+}
